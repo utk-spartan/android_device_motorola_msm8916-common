5c1f8b09536241f3ba90cd397e68e02f8f1e343e
axxx007xxxz <mattia-sky@hotmail.it>
2016-08-10 02:09:56 +0200

    BoardConfigCommon.mk: Fix qcom/sepolicy directory

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -100,7 +100,7 @@ TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
 
 # SELinux
-include device/qcom/sepolicy/sepolicy.mk
+include device/qcom/sepolicy/Android.mk
 BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy
 
 # Wifi
